9b1cb9bceb3d7945960c05efa94d7e86aa13811a
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2025-10-06 21:59:03 -0400

    Initial commit: estrutura base do projeto Aurora Fit

---
+++ config/settings/development.py
@@ -0,0 +1,5 @@
+﻿from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ config/settings/production.py
@@ -0,0 +1,15 @@
+﻿from .base import *
+from decouple import config
+
+DEBUG = False
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+# Security
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
